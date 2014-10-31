17180127882eb0b192cb048ce3078ac115db900b
Sam Dutton <dutton@google.com>
2014-10-31 11:09:22 +0000

    embed.org room working

--- js/test.js
+++ js/test.js
@@ -1,22 +1,33 @@
 'use strict';
 
-var websocket = new WebSocket('ws://echo.websocket.org/');
+var button = document.querySelector('button');
+var div = document.querySelector('div#receivedMessage');
+var input = document.querySelector('input');
+
+// var websocket = new WebSocket('wss://echo.websocket.org/');
+
+// doesn't work with wss :^|
+var websocket = new WebSocket('ws://sockets.mbed.org/ws/foo/rw');
 
 websocket.onopen = function(event) {
-  console.log(event);
-  var message = 'This is a test message';
-  websocket.send(message);
-}
+  console.log('open', event);
+};
 
 websocket.onclose = function(event) {
-  console.log(event);
+  console.log('close', event);
 };
 
 websocket.onmessage = function(event) {
-  console.log(event, event.data);
+  console.log('message', event, event.data);
+  div.textContent += event.data + ' ';
 };
 
 websocket.onerror = function(event) {
-  console.log('Error: '+ event.data);
+  console.log('error', event.data);
 };
 
+function send(message) {
+  websocket.send(message);
+}
+
+button.onclick = function(){send(input.value);};
